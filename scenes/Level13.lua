78d38df53ced687cdd560e624788e9bbfece03aa
Ben Finch <ben@Finch-Family-MacBook-Air.local>
2015-07-28 09:25:28 -0500

    added levels

---
+++ scenes/Level13.lua
@@ -0,0 +1,24 @@
+local level13 = {
+	worm = {
+		x = 469,
+		y = 397
+	},
+	foodTruck = {
+		shieldWorm = 90,
+		poisonWorm = 100
+	},
+	activators = {
+		{420, 663},
+		{686, 659},
+		{396, 521},
+		{569, 496},
+		{193, 458},
+		{761, 432},
+		{336, 387},
+		{442, 272}
+	},	
+	secondsAllowed = 60,
+	lengthObjective = 30
+}
+
+return level13
