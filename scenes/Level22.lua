a5eb5b4b0368cc9c50fbc33f76ce7754bab4448d
Ben Finch <ben@Finch-Family-MacBook-Air.local>
2015-09-12 12:43:17 -0500

    added new levels

---
+++ scenes/Level22.lua
@@ -0,0 +1,18 @@
+local level = {
+	worm = {
+		x = 100,
+		y = 374
+	},
+	foodTruck = {
+		standardWorm = 100
+	},
+	flyingActivators = {
+		velocity = 0.75,
+		frequency = 1000
+	},
+	secondsAllowed = 60,
+	lengthObjective = 30,
+	instructions = "Watch out for the ninja stars!"
+}
+
+return level
