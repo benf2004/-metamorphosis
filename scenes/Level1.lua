dbfac4122be2f5fcef7db83000927d0db3db4647
Ben Finch <ben@Finch-Family-MacBook-Air.local>
2015-12-19 08:25:34 -0600

    I have no idea what changed

--- scenes/Level1.lua
+++ scenes/Level1.lua
@@ -7,12 +7,10 @@ local level = {
 		standardWorm = 100
 	},
 	secondsAllowed = 60,
-<<<<<<< HEAD
+	stars = {60, 50, 40},
 	lengthObjective = 30,
-=======
-	lengthObjective = 10,
 	instructions = "Meet Wormy. He's very hungry! Drag him with your finger to feed him."
->>>>>>> 7aba63c96facece5954a36cbc9e0cd5990cb75b4
+
 }
 
 return level
