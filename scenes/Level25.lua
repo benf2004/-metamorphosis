a2e9e5d75d17001514df5bb512bf083ae86beac4
Greg Finch <finchgregoryc@johndeere.com>
2016-01-25 19:19:11 -0600

    new level 25

--- scenes/Level25.lua
+++ scenes/Level25.lua
@@ -2,22 +2,34 @@ local level = {
 	title = "Fire Trap",
 	worm = {
 		x = 100,
-		y = 374
+		y = 374,
+
 	},
 	foodTruck = {
-		standardWorm = 100
-	},
-	standardFood = {
-		{x=512, y=768-100, count=30, delay=1000},
-		{x=512, y=384, count=30, delay=10000},
-		{x=512, y=192, count=30, delay=25000}
+		standardWorm = 96,
+		shieldWorm = 2,
+		clockWorm = 2
 	},
 	miniFireSpouts = {
-	 {x = 512, y = 384, rotation = 0, rotate = true}
+	 {x = 512, y = 384, rotation = 0, rotate = true},
+	 {x = 156, y = 384, rotation = 45, rotate = true},
+	 {x = 889, y = 384, rotation = 90, rotate = true},
+	 {x = 512, y = 384, rotation = 135, rotate = true},
+	 {x = 512, y = 128, rotation = 190, rotate = true},
+	 {x = 512, y = 612, rotation = 235, rotate = true},
+	 {x = 889, y = 612, rotation = 280, rotate = true},
+	 {x = 889, y = 128, rotation = 315, rotate = true},
+	 {x = 156, y = 128, rotation = 360, rotate = true},
+	 {x = 156, y = 612, rotation = 45, rotate = true} 
+
+
+
+
 
 	},
 	secondsAllowed = 60,
-	lengthObjective = 80,
+	lengthObjective = 25,
+	sceneEffect = "midnight",
 	instructions = "Yum!"
 }
 
